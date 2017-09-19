9fb90dc38609cd7b2a5d723b0c0a9036b05a71f9
James Patrizi <jdp99@case.edu>
2017-09-19 00:04:58 -0400

    made progress learning how to interact with Toast and UI

--- app/src/main/java/com/rtl_pi/jamespatrizi/rtl_pi/MainActivity.java
+++ app/src/main/java/com/rtl_pi/jamespatrizi/rtl_pi/MainActivity.java
@@ -2,28 +2,37 @@ package com.rtl_pi.jamespatrizi.rtl_pi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button display;
+
+    private Toast toastFromEditText(EditText text) {
+        return Toast.makeText(this, text.getText().toString(), Toast.LENGTH_LONG);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        display = (Button) findViewById(R.id.button);
+        display.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText text = (EditText)findViewById(R.id.editText);
+                Toast toast = new Toast(getApplicationContext());
+                toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                toast = toastFromEditText(text);
+                toast.show();
+            }
+        });
 
-    // Example of a call to a native method
-    TextView tv = (TextView) findViewById(R.id.sample_text);
-    tv.setText(stringFromJNI());
-    }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
-
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
     }
 }
+
