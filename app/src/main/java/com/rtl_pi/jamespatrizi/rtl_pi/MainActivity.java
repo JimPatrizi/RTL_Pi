2342e1eb01a1c02ad92c37fb5452796af02a5c7e
James Patrizi <jdp99@case.edu>
2017-09-26 16:16:04 -0400

    added spinner with text and made dropdown height size to an acceptable touch level

--- app/src/main/java/com/rtl_pi/jamespatrizi/rtl_pi/MainActivity.java
+++ app/src/main/java/com/rtl_pi/jamespatrizi/rtl_pi/MainActivity.java
@@ -4,13 +4,16 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button display;
+    Button execute_button;
+    private String[] modeSpinner;
 
     private Toast toastFromEditText(EditText text) {
         return Toast.makeText(this, text.getText().toString(), Toast.LENGTH_LONG);
@@ -19,8 +22,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        display = (Button) findViewById(R.id.execute);
-        display.setOnClickListener(new View.OnClickListener() {
+
+        //Create Modulation Mode Spinner
+        this.modeSpinner = new String[] {
+                "WBFM", "AM", "USB", "LSB"
+        };
+        Spinner s = (Spinner) findViewById(R.id.modeSpinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, modeSpinner);
+        s.setAdapter(adapter);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //end spinner
+
+        //Create execute button listener
+        execute_button = (Button) findViewById(R.id.execute);
+        execute_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 EditText text = (EditText)findViewById(R.id.editText);
@@ -29,9 +45,7 @@ public class MainActivity extends AppCompatActivity {
                 toast = toastFromEditText(text);
                 toast.show();
             }
-        });
-
-
-    }
+        });//end execute listener
+    }//end onCreate
 }
 
