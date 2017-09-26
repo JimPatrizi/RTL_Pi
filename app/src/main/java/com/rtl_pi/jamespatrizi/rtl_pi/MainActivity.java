f117c0bd8b9395de27486803a9f0975558536952
James Patrizi <jdp99@case.edu>
2017-09-26 15:09:33 -0400

    made input freq and freq buttons

--- app/src/main/java/com/rtl_pi/jamespatrizi/rtl_pi/MainActivity.java
+++ app/src/main/java/com/rtl_pi/jamespatrizi/rtl_pi/MainActivity.java
@@ -1,12 +1,11 @@
 package com.rtl_pi.jamespatrizi.rtl_pi;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
